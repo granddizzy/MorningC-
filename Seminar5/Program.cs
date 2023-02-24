648dcb3ee53b3b547862f1a1078dffe56bf06bb5
roman lepihov <lepihovrom@yandex.by>
2023-02-24 22:33:04 +0300

    add: task 34

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -204,6 +204,49 @@ Console.Write("Count elements: " + count);
 // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i=0; i<size; i++)
+    {
+        array[i] =  new Random().Next(minValue, maxValue+1);
+    }
+
+    return array;
+}
+
+int CountОfEvenNumbers(int[] arr)
+{
+    int count=0;
+
+    for (int i=0; i<arr.Length; i++)
+        if (arr[i] % 2 == 0) count++;
+
+    return count;
+}
+
+void ShowArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRandomArray(size, 100, 999);
+
+int count = CountОfEvenNumbers(arr);
+
+ShowArray(arr);
+
+Console.Write("Number of even numbers: " + count);
+*/
+
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
