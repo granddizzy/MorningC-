589824c32cb209d63dccd53b7c17bf4c6a4d7696
roman lepihov <lepihovrom@yandex.by>
2023-02-24 23:08:26 +0300

    add: task 38

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -301,3 +301,54 @@ Console.Write("Sum: "+sum);
 
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i=0; i<size; i++)
+        array[i] =  new Random().Next(minValue, maxValue+1);
+
+    return array;
+}
+
+void ShowArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+
+    Console.WriteLine();
+}
+
+int GetDifferenceMaxMin(int[] arr)
+{
+    int max=arr[0];
+    int min=arr[0];
+
+    for (int i=1; i<arr.Length; i++)
+    {
+        if (arr[i]>max) max=arr[i];
+        if (arr[i]<min) min=arr[i];
+    }
+
+    return max-min;
+}
+
+Console.Write("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input array min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input array max value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRandomArray(size, min, max);
+
+ShowArray(arr);
+
+int diff = GetDifferenceMaxMin(arr);
+
+Console.WriteLine("Difference max value and min value of array: "+diff);
+*/
