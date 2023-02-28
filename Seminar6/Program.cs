e839f283cf5c7667c0efc743f750881ba2e64e7f
roman lepihov <lepihovrom@yandex.by>
2023-02-28 19:21:45 +0300

    add: task 41

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -123,7 +123,6 @@ ShowArray(arr);
 ShowArray(CopyArray(arr));
 */
 
-
 //Напишите программу, 
 //которая будет преобразовывать десятичное число в двоичное.
 
@@ -145,3 +144,40 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(ConvertToBin(number));
 */
+
+//////////////////////////////////////////////
+//HOME WORK
+//////////////////////////////////////////////
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*
+int CountOfPositiveNumbers(int m)
+{
+    int count=0;
+    for (int i=1; i<=m; i++)
+    {
+        Console.Write($"Input number {i}: ");
+        if (Convert.ToInt32(Console.ReadLine()) > 0) count++;
+    }
+
+    return count;
+}
+
+int m=0;
+while (m<=0) 
+{
+    Console.Write("Input positive integer M: ");
+    m = Convert.ToInt32(Console.ReadLine());
+
+    if (m<=0) Console.Write("Incorrect M !!! ");
+}
+
+Console.Write("Count of positive numbers: "+CountOfPositiveNumbers(m));
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
