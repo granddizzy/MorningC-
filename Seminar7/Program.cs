44927448ce011b44e76d9c74656a5f2f354dd0aa
roman lepihov <lepihovrom@yandex.by>
2023-03-03 14:51:07 +0300

    add: task 47

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -111,3 +111,64 @@ int sum = GetSumDiagonal(arr);
 Show2dArray(arr);
 Console.WriteLine(sum);
 */
+
+/////////////////////////////////////////////////////////////////
+//HOME WORK
+/////////////////////////////////////////////////////////////////
+
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Show2dDoubleArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j=0; j<arr.GetLength(1); j++)
+            Console.Write(arr[i,j] + " ");
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+double[,] CreateRandom2dDoubleArray()
+{
+    int rows = 0;  
+    while (rows<=0)
+    {
+        Console.Write("Input a number rows:");
+        rows = Convert.ToInt32(Console.ReadLine());
+        if (rows<=0) Console.Write("Invald rows. ");
+    }
+
+    int cols = 0;  
+    while (cols<=0)
+    {
+        Console.Write("Input a number cols:");
+        cols = Convert.ToInt32(Console.ReadLine());
+        if (cols<=0) Console.Write("Invald cols. ");
+    }
+
+    Console.Write("Input a minValue:");
+    double minValue = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Input a maxValue:");
+    double maxValue = Convert.ToDouble(Console.ReadLine());
+
+    double[,] array = new double[rows, cols];
+
+    for (int i=0; i<rows; i++)
+        for (int j=0; j<cols; j++)
+            array[i,j] =  Math.Round(new Random().NextDouble() * (maxValue - minValue) + maxValue, 2);
+
+    return array;
+}
+
+/*
+double[,] arr = CreateRandom2dDoubleArray();
+Show2dDoubleArray(arr);
+*/
