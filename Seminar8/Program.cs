2bb8bb5db61c06dc569505c7704a9dfc39e57a97
roman lepihov <lepihovrom@yandex.by>
2023-03-07 13:40:02 +0300

    add: task 54

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -172,3 +172,39 @@ arr = ChangeArray(arr, minIndexex[0], minIndexex[1]);
 
 Show2dArray(arr);
 */
+
+///////////////////////////////////////////////////////////////
+//HOME WORK
+///////////////////////////////////////////////////////////////
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+/*
+void SortDesc(int[,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+        for (int j=0; j<arr.GetLength(1)-1; j++)
+            for (int k=0; k<arr.GetLength(1)-1-j; k++)
+            {
+                if (arr[i,k+1]>arr[i,k])
+                {
+                    int tmp = arr[i,k];
+                    arr[i,k] = arr[i,k+1];
+                    arr[i,k+1]=tmp;
+                }
+            }
+}
+
+int[,] arr = CreateRandom2dArray();
+Show2dArray(arr);
+SortDesc(arr);
+Show2dArray(arr);
+*/
