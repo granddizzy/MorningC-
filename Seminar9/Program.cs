8544389615eefd9fac2912b776ad73505b26d1f4
roman lepihov <lepihovrom@yandex.by>
2023-03-13 18:43:13 +0300

    add: task 68

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -115,3 +115,28 @@ int sumNaturalElements = GetSumNaturalElements(n, m);
 
 Console.WriteLine($"sum of natural elements between {n} and {m} is {sumNaturalElements}");
 */
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+/*
+int AcermanFunction(int m, int n)
+{   
+    if (m==0) return n+1;
+
+    if (m!= 0 && n==0) return AcermanFunction(m-1, 1);
+
+    if (m>0 && n>0) return AcermanFunction(m-1, AcermanFunction(m, n-1));
+
+    return AcermanFunction(m, n);
+}
+
+Console.Write("Input M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = AcermanFunction(m, n);
+Console.WriteLine(a);
+*/
