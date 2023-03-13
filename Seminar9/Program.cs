7514122056bc4b74f109b42bdb00eee7fc17641d
roman lepihov <lepihovrom@yandex.by>
2023-03-13 17:04:56 +0300

    add: task 64

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -91,3 +91,27 @@ Console.Write("Input N:");
 int n = Convert.ToInt32(Console.ReadLine());
 ShowNaturalNumbers(n);
 */
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/*
+int GetSumNaturalElements(int n, int m)
+{
+    if (n>m+1)
+        return m + 1 + GetSumNaturalElements(n, m+1);
+     else if (n+1<m)
+         return n + 1 + GetSumNaturalElements(n+1, m);
+
+    return 0;
+}
+
+Console.Write("Input N:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input M:");
+int m = Convert.ToInt32(Console.ReadLine());
+int sumNaturalElements = GetSumNaturalElements(n, m);
+
+Console.WriteLine($"sum of natural elements between {n} and {m} is {sumNaturalElements}");
+*/
